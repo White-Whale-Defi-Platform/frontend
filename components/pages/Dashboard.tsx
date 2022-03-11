edb2e5338ce84c1c8d9b778ed15096de0999c105
vindhum <vinodhum@outlook.com>
2022-03-11 14:45:41 -0500

    dashboard chart size fix

--- components/pages/Dashboard.tsx
+++ components/pages/Dashboard.tsx
@@ -17,34 +17,34 @@ const Dashboard: NextPage = () => {
   const { getIcon, getSymbol } = useTokenInfo();
   const vUSTPrice = usevUSTPrice();
   // console.log(`vUSTpriceinfo  ${vUSTPrice}`);
-  
+
   return (
     <Box my="16" mx="auto" maxW="container.xl">
       <Heading color="#fff" size="lg" mb="10">
         Dashboard
-        </Heading>
-        <Flex align="end" justify="end" mt="0" mb="2">
-            {vUSTPrice && (
-                <HStack justify="space-between" color="#fff" spacing="2">
-                  
-                  <Tag
-                    size={'lg'}
-                    key={'lg'}
-                    borderRadius='full'
-                    variant='solid'
-                    colorScheme='brand.400'
-                    
-                  >
-                  {/* TODO: There probably shouldn't be so many boxes here. There is defo a cleaner way to do this ? */}
-                  <Box m="1">1 </Box><Box m="1">  vUST</Box> <VUSTExchangeIcon token="vUST" />{' '}
-                  <Box m="1">≈</Box>{' '}
-                  <Box m="1">{vUSTPrice}{' '}</Box>
-                  <Box m="1">UST</Box> <USTExchangeIcon token="ust" />
-                  </Tag>
-                  </HStack>
+      </Heading>
+      <Flex align="end" justify="end" mt="0" mb="2">
+        {vUSTPrice && (
+          <HStack justify="space-between" color="#fff" spacing="2">
+
+            <Tag
+              size={'lg'}
+              key={'lg'}
+              borderRadius='full'
+              variant='solid'
+              colorScheme='brand.400'
+
+            >
+              {/* TODO: There probably shouldn't be so many boxes here. There is defo a cleaner way to do this ? */}
+              <Box m="1">1 </Box><Box m="1">  vUST</Box> <VUSTExchangeIcon token="vUST" />{' '}
+              <Box m="1">≈</Box>{' '}
+              <Box m="1">{vUSTPrice}{' '}</Box>
+              <Box m="1">UST</Box> <USTExchangeIcon token="ust" />
+            </Tag>
+          </HStack>
         )}
-        </Flex>
-      
+      </Flex>
+
 
       <Grid templateColumns="repeat(12, 1fr)" gap={{ base: 2, md: 12 }}>
         <GridItem colSpan={{ base: 12, lg: 5 }}>
@@ -53,12 +53,8 @@ const Dashboard: NextPage = () => {
 
         <GridItem colSpan={{ base: 12, lg: 7 }}>
           <GridItem rowSpan={{ base: 3, lg: 3 }}>
-        
-    </GridItem>
-          <WhaleCard />
-        </GridItem>
-        <GridItem colSpan={{ base: 12, lg: 12 }}>
-          {/* <TransactionListCard/> */}
+            <WhaleCard />
+          </GridItem>
         </GridItem>
       </Grid>
     </Box>
